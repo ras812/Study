94ca5bc300a595ff9ff41f20455cceaf4f3263a4
ras812 <69597319+ras812@users.noreply.github.com>
2020-10-12 11:55:43 +0300

    squirrel add
    squirrel add

---
+++ Squirrel/Squirrel/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squirrel
+{
+    class Program
+    {
+        public static int Squirrel(int n)
+        {
+            // факториал
+            int factorial = 1;
+            for (int i = 1; i < (n + 1); i++)
+            {
+                factorial = factorial * i;
+            }
+
+            // первое число символа
+            int m = factorial;
+            while (m != m % 10)
+            {
+                m = m / 10;
+            }
+
+            // Console.WriteLine("Первая цифра факториала: {0}", m);
+            // Console.WriteLine("Факториал числа !{0} равен: {1}", n, factorial);
+            return m;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Squirrel(10));
+            Console.ReadKey();
+        }
+    }
+}
