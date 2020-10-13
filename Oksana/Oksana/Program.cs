3ef090e4e3c3511095fa7737256d8afc64d05fa1
ras812 <69597319+ras812@users.noreply.github.com>
2020-10-14 01:09:15 +0300

    add oksana

---
+++ Oksana/Oksana/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level1Space
+{
+    public static class Level1
+    {
+        public static int odometer(int[] oksana)
+        {
+            int distance = 0;
+            
+            for (int i = 0; i < oksana.Length; i++)
+            {
+                if (i < 2 && i % 2 == 1)
+                {
+                    distance = oksana[i] * oksana[i-1];
+                }
+                else if (i % 2 == 1)
+                {
+                    distance = distance + (oksana[i - 1] * (oksana[i] - oksana[i - 2]));
+                }
+            }
+            return distance;
+        }
+    
+        /*static void Main(string[] args)
+        {
+            int[] arr = {20, 1, 30, 4};
+            Console.WriteLine(odometer(arr));
+            Console.ReadKey();
+        }*/
+    }
+}
