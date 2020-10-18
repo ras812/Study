3c49acf2f54c06a01d8d2019d54ee0e33effd139
ras812 <69597319+ras812@users.noreply.github.com>
2020-10-18 18:44:47 +0300

    ch Conq

--- ConquestCampaign/ConquestCampaign2/Program.cs
+++ ConquestCampaign/ConquestCampaign2/Program.cs
@@ -8,7 +8,6 @@ namespace ConquestCampaign
 {
     public static class Level1
     {
-
         public static int ConquestCampaign(int N, int M, int L, int[] battalion)
         {
             int[,] battleGround = new int[N, M];
@@ -25,123 +24,7 @@ namespace ConquestCampaign
                     battleGround[i, j] = 0;
                 }
             }
-            #region
-            // FUNCTIONS
-
-            // PrintBattleGround
-            /*void PrintBattleGround(int[,] arr)
-            {
-                Console.WriteLine("Today is {0} battle day", countDays);
-
-                for (int i = 0; i < battleGround.GetLength(0); i++)
-                {
-
-                    for (int j = 0; j < battleGround.GetLength(1); j++)
-                    {
-                        Console.Write(battleGround[i, j] + " ");
-                    }
-                    Console.WriteLine();
-                }
-                Console.WriteLine();
-            }*/
-            // end PrintBattleGround
-
-            // Captured
-            /*int Captured(int[,] arr)
-            {
-
-                for (int i = 0; i < battleGround.GetLength(0); i++)
-                {
-
-                    for (int j = 0; j < battleGround.GetLength(1); j++)
-                    {
-
-                        if (arr[i, j] == 0)
-                        {
-                            return 0; // not captured
-                        }
-                    }
-                }
-                return 1;
-            }*/
-            // end Captured
-
-            // Battle
-            /*int[,] Battle(ref int[,] arr)
-            {
-
-                for (int i = 0; i < arr.GetLength(0); i++)
-                {
-
-                    for (int j = 0; j < arr.GetLength(1); j++)
-                    {
-
-                        if (arr[i, j] == 2)
-                        {
-
-                            if ((i - 1) >= 0 && (i - 1) < arr.GetLength(0))
-                            {
-
-                                if (arr[i - 1, j] == 0)
-                                {
-                                    arr[i - 1, j] = 1; // up
-                                }
-                            }
-
-                            if ((i + 1) >= 0 && (i + 1) < arr.GetLength(0))
-                            {
-
-                                if (arr[i + 1, j] == 0)
-                                {
-                                    arr[i + 1, j] = 1; // down
-                                }
-                            }
-
-                            if ((j - 1) >= 0 && (j - 1) < arr.GetLength(1))
-                            {
-
-                                if (arr[i, j - 1] == 0)
-                                {
-                                    arr[i, j - 1] = 1; //left
-                                }
-                            }
-
-                            if ((j + 1) >= 0 && (j + 1) < arr.GetLength(1))
-                            {
-
-                                if (arr[i, j + 1] == 0)
-                                {
-                                    arr[i, j + 1] = 1; // right
-                                }
-                            }
-                        }
-                    }
-                }
-
-                for (int i = 0; i < arr.GetLength(0); i++)
-                {
-
-                    for (int j = 0; j < arr.GetLength(1); j++)
-                    {
-
-                        if (arr[i, j] == 1)
-                        {
-                            arr[i, j] = 2;
-                        }
-
-                        else if (arr[i, j] == 2)
-                        {
-                            arr[i, j] = 3;
-                        }
-                    }
-                }
-
-                return arr;
-            }*/
-            // end Battle
-
-            // END FUNCTIONS
-            #endregion
+            
             // 0 - not captured
             // 1 - marked for catpure
             // 2 - captured and catpure from this point
@@ -246,7 +129,7 @@ namespace ConquestCampaign
                 
 
                 countDays++;
-                // PrintBattleGround(battleGround);
+                // PrintBattleGround
 
                 /*Console.WriteLine("Today is {0} battle day", countDays);
 
