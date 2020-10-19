f75a251cad76006e07ade90c35973cf4e16099a6
ras812 <69597319+ras812@users.noreply.github.com>
2020-10-19 23:07:14 +0300

    ch Max

--- MadMax/MadMax/Program.cs
+++ MadMax/MadMax/Program.cs
@@ -17,38 +17,9 @@ namespace Level1Space
                 return Tele;
             }
 
-            else if (Tele.Length == 3)
+            if (Tele.Length >= 3)
             {
-                for (int i = 0; i < Tele.Length; i++)
-                {
-                    for (int j = 0; j < Tele.Length - 1; j++)
-                    {
-                        if (Tele[j] >= Tele[j + 1])
-                        {
-                            temp = Tele[j];
-                            Tele[j] = Tele[j + 1];
-                            Tele[j + 1] = temp;
-                        }
-                    }
-                }
-                temp = Tele[1];
-                Tele[1] = Tele[2];
-                Tele[2] = temp;
-
-                // print
-                /*Console.WriteLine();
-                for (int k = 0; k < Tele.Length; k++)
-                {
-                    Console.Write(Tele[k] + " ");
-                }*/
-                // end print
-
-                return Tele;
-            }
-
-            else
-            {
-                for (int i = 0; i < Tele.Length; i++)
+                for (int i = 0; i < Tele.Length; i++) //sort by higher
                 {
                     for (int j = 0; j < Tele.Length - 1; j++)
                     {
@@ -61,27 +32,61 @@ namespace Level1Space
                     }
                 }
 
-                for (int i = 0; i < (Tele.Length / 2) / 2; i++)
+                if (Tele.Length == 3)
                 {
-                    temp = Tele[(Tele.Length / 2) + i];
-                    Tele[(Tele.Length / 2) + i] = Tele[Tele.Length - 1 - i];
-                    Tele[Tele.Length - 1 - i] = temp;
+                    temp = Tele[1];
+                    Tele[1] = Tele[2];
+                    Tele[2] = temp;
+
+                    // print
+                    /*Console.WriteLine();
+                    for (int k = 0; k < Tele.Length; k++)
+                    {
+                        Console.Write(Tele[k] + " ");
+                    }*/
+                    // end print
                 }
 
-                // print
-                /*Console.WriteLine();
-                for (int k = 0; k < Tele.Length; k++)
+                else
                 {
-                    Console.Write(Tele[k] + " ");
-                }*/
-                // end print
-
-                return Tele;
+                    if ((Tele.Length / 2) % 2 == 0) //difference in iterations
+                    {
+                        for (int i = 0; i < (Tele.Length / 2) / 2; i++) // here!!!
+                        {
+                            temp = Tele[(Tele.Length / 2) + i];
+                            Tele[(Tele.Length / 2) + i] = Tele[Tele.Length - 1 - i];
+                            Tele[Tele.Length - 1 - i] = temp;
+                            // print
+                            /*Console.WriteLine();
+                            for (int k = 0; k < Tele.Length; k++)
+                            {
+                                Console.Write(Tele[k] + " ");
+                            }*/
+                            // end print
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < ((Tele.Length / 2) / 2) + 1; i++)   // and here!!!
+                        {
+                            temp = Tele[(Tele.Length / 2) + i];
+                            Tele[(Tele.Length / 2) + i] = Tele[Tele.Length - 1 - i];
+                            Tele[Tele.Length - 1 - i] = temp;
+                            // print
+                            /*Console.WriteLine();
+                            for (int k = 0; k < Tele.Length; k++)
+                            {
+                                Console.Write(Tele[k] + " ");
+                            }*/
+                            // end print
+                        }
+                    }
+                }
             }
-
+            return Tele;
         }
 
-        static void Main(string[] args)
+        /*static void Main(string[] args)
         {
             int[] madMaxArr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
             //MadMax(9, madMaxArr);
@@ -99,6 +104,6 @@ namespace Level1Space
 
 
             Console.ReadKey();
-        }
+        }*/
     }
 }
