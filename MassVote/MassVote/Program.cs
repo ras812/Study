2c4f98e0404eaa4cd6644893716a2ab471b29e93
ras812 <69597319+ras812@users.noreply.github.com>
2021-02-09 00:32:21 +0300

    add Votes

---
+++ MassVote/MassVote/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MassVote
+{
+    public static class Vote
+    {
+        public static string MassVote(int N, int[] Votes)
+        {
+            string rezStr = null;
+            int flag = 0;
+
+            if (Votes.Length == 1)
+            {
+                rezStr = "majority winner 1";
+            }
+            else
+            {
+                if()
+            }
+
+
+            for (int i = 0; i < Votes.Length; i++)
+            {
+                for (int j = 0; j < Votes.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    if (Votes[i] == Votes[j])
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(flag);
+
+            Console.WriteLine(rezStr);
+            return null;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Vot = { 10, 15, 10, 5 };
+            Vote.MassVote(4, Vot);
+            Console.ReadKey();
+        }
+    }
+}
